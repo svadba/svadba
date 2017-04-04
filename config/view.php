1bccdd9a69004e2e9e1d63434b946fed8ee46de2
svadba <berwcl@gmail.com>
2017-04-04 15:50:44 +0600

    ну xnj

--- config/view.php
+++ config/view.php
@@ -31,3 +31,14 @@ return [
     'compiled' => realpath(storage_path('framework/views')),
 
 ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Compiled View Path
+    |--------------------------------------------------------------------------
+    |
+    | This option determines where all the compiled Blade templates will be
+    | stored for your application. Typically, this is within the storage
+    | directory. However, as usual, you are free to change this value.
+    |
+    */
